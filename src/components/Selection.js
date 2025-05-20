bb55e31d55e548332361abcbe2cf078ec9776e6b
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-05-20 22:29:47 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
 
+import React, { useState } from 'react'
+
+const Selection = (props) => {
+  const [bgStyle, setBgStyle] = useState({});
+  const handleClick = () => {
+    props.applyColor(setBgStyle);
+  };
+  return (
+    <div
+      className="fix-box"
+      style={bgStyle}
+      onClick={handleClick}
+    >
+    </div>
+  )
+}
+
+export default Selection
